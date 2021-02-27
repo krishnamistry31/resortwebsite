a0553cd9ee7d90ea3094a5dd329e191cda375945
krishnam@sharpcodesolutions.net <krishnam@sharpcodesolutions.net>
2021-02-27 22:21:03 +0530

    activities page done

--- activities.php
+++ activities.php
@@ -1,10 +1,85 @@
 <html>
     <head>
     <link rel="stylesheet" href="css/gallery.css">
+    <link rel="stylesheet" href="css/activities.css">
     </head>
     <body>
         <div style="margin-left:100px;margin-top:130px">
             <h2>Activities</h2>            
         </div>
+        <div style="margin: 30px 100px;border:1px solid grey;border-radius: 10px">
+            <p class="para">
+            At the Oasis Camp our all Activities are fear free zone. You can enjoy with your friends, family and couples. Our activities like. Country’s Longest  Zip line, Extremely design high rope Adventure Zone on 50 feet height, Kayaking, Eco go cart, Flying Fox, Tyre tumble, Orsang bridge,Sky walk, Monkey bridge , Zig zag, Swing zag, Commando net, Swing bridge, Burma bridge,Bird watching, Swimming pool, Waterfall, Fun with PET Iguana,  Kids Adventure zone, Trekking , Jungle Cooking and many more.
+            </p>
+        </div>
+        <table width="90%" align="center">
+            <tr>
+                <td>
+                    <div class="flip-card">
+                        <div class="flip-card-inner">
+                            <div class="flip-card-front">
+                            <img src="imgs\trampoline.jpg" alt="Avatar" style="width:400px;height:300px;">
+                            </div>
+                            <div class="flip-card-back">
+                            <h2>Trampoline</h2>                             
+                            </div>
+                        </div>
+                    </div>
+                </td>
+                <td>
+                    <div class="flip-card">
+                        <div class="flip-card-inner">
+                            <div class="flip-card-front">
+                            <img src="imgs\trampoline.jpg" alt="Avatar" style="width:400px;height:300px;">
+                            </div>
+                            <div class="flip-card-back">
+                            <h2>Swimming Pool</h2>                             
+                            </div>
+                        </div>
+                    </div>
+                </td>
+                <td>
+                    <div class="flip-card">
+                        <div class="flip-card-inner">
+                            <div class="flip-card-front">
+                            <img src="imgs\trampoline.jpg" alt="Avatar" style="width:400px;height:300px;">
+                            </div>
+                            <div class="flip-card-back">
+                            <h2>Raft Rounder</h2>                             
+                            </div>
+                        </div>
+                    </div>
+                </td>
+             </tr>       
+             <tr>
+                <td>
+                    <div class="flip-card">
+                        <div class="flip-card-inner">
+                            <div class="flip-card-front">
+                            <img src="imgs\trampoline.jpg" alt="Avatar" style="width:400px;height:300px;">
+                            </div>
+                            <div class="flip-card-back">
+                            <h2>Camp Fire</h2>                             
+                            </div>
+                        </div>
+                    </div>
+                </td>
+                <td>
+                    <div class="flip-card">
+                        <div class="flip-card-inner">
+                            <div class="flip-card-front">
+                            <img src="imgs\trampoline.jpg" alt="Avatar" style="width:400px;height:300px;">
+                            </div>
+                            <div class="flip-card-back">
+                            <h2>Adven</h2>                             
+                            </div>
+                        </div>
+                    </div>
+                </td>
+                <td>
+                </td>
+             </tr>                                
+                         
+            </table>
     </body>
 </html>
